60f19a61acbdbd119ab286b0a34cd6db9f8d1ac3
nasteew <anastasiya.w53@gmail.com>
2026-05-09 19:02:11 +0300

    test: add mocks for card and card list

---
+++ src/test-utils/mocks/mockItem.ts
@@ -0,0 +1,9 @@
+import type { Item } from '@/types/item';
+
+export const mockItem: Item = {
+  name: 'bulbasaur',
+  image: 'https://example.com/bulbasaur.png',
+  types: ['grass', 'poison'],
+  height: 7,
+  weight: 69,
+};
