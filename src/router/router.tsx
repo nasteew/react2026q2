3a45ea048f8abcb9fd280796245b030db744b1fb
nasteew <anastasiya.w53@gmail.com>
2026-05-17 17:20:29 +0300

    feat: create routes

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { AppLayout } from '../layout/AppLayout';
-import { Home } from '../pages/Home/Home';
+import { Home } from '@/pages/Home/Home';
+import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
 import { About } from '../pages/About/About';
 import { NotFound } from '../pages/NotFound/NotFound';
 import { PokemonDetails } from '../pages/Details/PokemonDetails';
@@ -8,26 +9,16 @@ import { PokemonDetails } from '../pages/Details/PokemonDetails';
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <AppLayout />,
+    element: (
+      <ErrorBoundary>
+        <AppLayout />
+      </ErrorBoundary>
+    ),
     children: [
-      {
-        index: true,
-        element: <Home />,
-        children: [
-          {
-            path: 'pokemon/:id',
-            element: <PokemonDetails />,
-          },
-        ],
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
+      { index: true, element: <Home /> },
+      { path: 'pokemon/:id', element: <PokemonDetails /> },
+      { path: 'about', element: <About /> },
+      { path: '*', element: <NotFound /> },
     ],
   },
 ]);
