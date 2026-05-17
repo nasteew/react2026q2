e8ce45a0944603b324b612bb69beeb8c4e93aad2
nasteew <anastasiya.w53@gmail.com>
2026-05-17 17:26:21 +0300

    fix: update api types

--- src/types/item.ts
+++ src/types/item.ts
@@ -4,4 +4,8 @@ export interface Item {
   types: string[];
   height: number;
   weight: number;
+  abilities?: string[];
+  stats?: { name: string; value: number }[];
+  baseExperience?: number;
+  moves?: string[];
 }
--- src/types/pokemonApi.ts
+++ src/types/pokemonApi.ts
@@ -1,7 +1,10 @@
+import type { PokemonTypeEntry } from './pokemonType';
+
 export interface PokemonResponse {
   name: string;
   height: number;
   weight: number;
+
   sprites: {
     other: {
       'official-artwork': {
@@ -9,7 +12,23 @@ export interface PokemonResponse {
       };
     };
   };
+
   types: PokemonTypeEntry[];
+
+  abilities: {
+    ability: { name: string };
+  }[];
+
+  stats: {
+    base_stat: number;
+    stat: { name: string };
+  }[];
+
+  base_experience: number;
+
+  moves: {
+    move: { name: string };
+  }[];
 }
 
 export interface PokemonListItem {
