52ba65fce270848c5995c622e3051a6321660f75
nasteew <anastasiya.w53@gmail.com>
2026-05-18 22:17:35 +0300

    fix: update router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, redirect } from 'react-router-dom';
 import { AppLayout } from '../layout/AppLayout';
 import { Home } from '@/pages/Home/Home';
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
 import { About } from '../pages/About/About';
 import { NotFound } from '../pages/NotFound/NotFound';
-import PokemonDetails from '../pages/Details/PokemonDetails';
+import PokemonDetails from '../components/Details/PokemonDetails';
 
 export const router = createBrowserRouter([
   {
@@ -16,16 +16,25 @@ export const router = createBrowserRouter([
     ),
     children: [
       {
-        path: '',
+        path: '/',
         element: <Home />,
+        loader: ({ request }) => {
+          const url = new URL(request.url);
+
+          if (!url.searchParams.get('page')) {
+            url.searchParams.set('page', '1');
+            return redirect(url.pathname + url.search);
+          }
+
+          return null;
+        },
         children: [
           {
-            path: 'details/:id',
+            index: true,
             element: <PokemonDetails />,
           },
         ],
       },
-      { path: 'pokemon/:id', element: <PokemonDetails /> },
       { path: 'about', element: <About /> },
       { path: '*', element: <NotFound /> },
     ],
