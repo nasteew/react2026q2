a53b1c03fc86cd1718f539d4c302b9d3703c10be
nasteew <anastasiya.w53@gmail.com>
2026-06-22 21:31:21 +0300

    fix: csv files

--- src/proxy.ts
+++ src/proxy.ts
@@ -19,5 +19,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next|_vercel|.*\\..*).*)'],
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
 };
